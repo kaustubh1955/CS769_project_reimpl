0417ddd0a4d0f0a208866a62410984282b14dcca
Jeonghyun Park <tom0365@cau.ac.kr>
2025-07-20 13:56:05 +0900

    Update and rename Check_Initial_Rank.py to check_Initial_Rank.py

--- check_Initial_Rank.py
+++ check_Initial_Rank.py
@@ -1,8 +1,8 @@
 import json
 from collections import defaultdict
 
-input_file = "run.retrieve.top_50.mkqa_en.wiki-100w-multilingual-alllangs.dev.BAAI_bge-m3.trec"
-output_file = "en_initial_rank.json"
+input_file = "run.retrieve.top_50.mkqa_ko.wiki-100w-multilingual-alllangs.dev.BAAI_bge-m3.trec"
+output_file = "ko_en_initial_rank.json"
 
 query_results = defaultdict(list)
 
@@ -14,11 +14,12 @@ with open(input_file, 'r', encoding='utf-8') as infile:
             doc_id = parts[2]
             rank = int(parts[3])
             
+            if doc_id.startswith("kilt"):
+                continue
+            
             query_results[query_id].append({"doc_id": doc_id, "rank": rank})
 
 with open(output_file, 'w', encoding='utf-8') as outfile:
     json.dump(query_results, outfile, ensure_ascii=False, indent=4)
 
 print(f"Filtered results saved to {output_file}")
-
-
